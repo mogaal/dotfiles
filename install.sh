3e45885096b18a6037908efe7c8ffe0bc30c1a4c
Alejandro Garrido Mota <alejandro@debian.org>
2016-04-19 14:07:51 +0100

    Starting to adapt dotfiles to MacOSX

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BACKUP="$(dirname "$DOTFILES")/dotfiles_old"
 
+
 if [[ -d "$DOTFILES" ]]; then
   echo "Symlinking dotfiles from $DOTFILES"
 else
@@ -33,13 +34,21 @@ function BackupFile {
    mv $HOME/.$1 $BACKUP/$1
 }
 
-lnFile vim vim
-lnFile i3 i3
-lnFile vimrc vimrc 
-lnFile irssi irssi 
-lnFile bashrc bashrc
-lnFile bash_vars bash_vars 
-lnFile bash_aliases bash_aliases 
-lnFile gitconfig gitconfig 
-lnFile tmux.conf tmux.conf
-lnFile gbp.conf gbp.conf
+
+# Things to install in base of the OS
+if [[ "$OSTYPE" == darwin* ]]; then
+  echo "Doing nothing..."
+fi
+
+if [[ "$OSTYPE" == linux* ]]; then
+  lnFile vim vim
+  lnFile i3 i3
+  lnFile vimrc vimrc 
+  lnFile irssi irssi 
+  lnFile bashrc bashrc
+  lnFile bash_vars bash_vars 
+  lnFile bash_aliases bash_aliases 
+  lnFile gitconfig gitconfig 
+  lnFile tmux.conf tmux.conf
+  lnFile gbp.conf gbp.conf
+fi
