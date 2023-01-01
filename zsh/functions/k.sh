012563d6a10de6a939bcbe1186908482b026f0c0
Alejandro Garrido Mota <garridomota@gmail.com>
2023-01-01 15:11:50 +0000

    Remove old files

--- zsh/functions/k.sh
+++
@@ -1,8 +0,0 @@
-
-# Executing terraform init against Gitlab
-function k-tf-init {
-  terraform init \
-      -backend-config="username=$GITLAB_USERNAME" \
-      -backend-config="password=$GITLAB_ACCESS_TOKEN" \
-      -backend-config="retry_wait_min=5"
-}
--- zsh/functions/ume.sh
+++
@@ -1,6 +0,0 @@
-
-# Things related with work
-
-function decrypt {
-  docker run -v ~/workspace/kevin-puppet/keys:/keys/ -it --rm --name eyaml halberom/hiera-eyaml decrypt -s $1
-}
