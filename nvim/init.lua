e03f025d59dad775933bd66e866989352b19044e
Alejandro Garrido Mota <garridomota@gmail.com>
2026-02-10 15:07:18 +0000

    fix: nvim-treesitter setup in init.lua
    Removed dependencies and configuration options for nvim-treesitter.

--- nvim/init.lua
+++ nvim/init.lua
@@ -243,28 +243,8 @@ require('lazy').setup({
   -- Highlight, edit, and navigate code
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true, disable = { 'python' } },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<M-space>',
-        },
-      },
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
+    lazy = false,
+    build = ':TSUpdate'
   },
 
   -- Theme
