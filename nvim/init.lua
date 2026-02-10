bc0781d8c9c4760e83164e524e4cd8f5394cad7f
Alejandro Garrido Mota <garridomota@gmail.com>
2026-02-10 15:00:00 +0000

    feat: refact nvim-treesitter configuration setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -246,25 +246,24 @@ require('lazy').setup({
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
-    config = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
-        auto_install = true,
-
-        highlight = { enable = true },
-        indent = { enable = true, disable = { 'python' } },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<M-space>',
-          },
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = { 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true, disable = { 'python' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<M-space>',
         },
-      }
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
     end,
   },
 
