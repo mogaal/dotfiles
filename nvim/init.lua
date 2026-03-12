bbe6b619938d246618fa6a215e960c227f583337
Alejandro Garrido Mota <134855+mogaal@users.noreply.github.com>
2026-03-12 15:26:57 +0000

    feat: update picker settings in init.lua
    Enhanced picker configuration with sources for grep and files.

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,7 +118,13 @@ require('lazy').setup({
       explorer = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        sources = {
+          grep = { hidden = true },
+          files = { hidden = true },
+        },
+      },
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
